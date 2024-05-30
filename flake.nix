5adc0e71df3d43e72c672faaf17311c539c54269
Guimarret <guimarret@gmail.com>
2024-05-30 20:17:58 -0300

    Flake to integrate poetry and spakr, also added the shell hook to work with jupyter notebooks

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Application packaged with jupyter and spark using NIX!!!!!";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+      in
+      {
+        packages = {
+          myapp = mkPoetryApplication { projectDir = self; };
+          default = self.packages.${system}.myapp;
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs;
+          [ 
+            poetry
+            spark
+          ];
+
+          shellHook = ''
+            export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
+              pkgs.stdenv.cc.cc
+            ]}
+          '';
+        };
+      });
+}
